fcc3eec3828a6c9ab526af4297af16052cb6d272
Felix Schinkel <FelixSchinkel@users.noreply.github.com>
2020-06-17 00:00:59 +0200

    fix pattern matching when no value to convert print empty string

--- src/components/ConvertPace.vue
+++ src/components/ConvertPace.vue
@@ -4,46 +4,43 @@
       <v-row>
         <v-text-field label="pace/mile" v-model="paceMile" :rules="inputRules" />
         <v-chip class="ma-2" label>&lt;-&gt;</v-chip>
-        <v-text-field label="pace/km" :value="convertMile2Km" :rules="inputRules"/>
+        <v-text-field label="pace/km" :value="convertMile2Km" :rules="inputRules" />
       </v-row>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+// regex checks if time format is: M.SS
+const pattern = /^([0-9][.,][0-5][0-9])$/;
+
 export default {
   data: () => ({
     paceMile: "",
     inputRules: [
-      value => !!value || 'Required.',
+      value => !!value || "Required.",
       value => {
-        // regex checks if time format is: M.SS
-        const pattern = /^([0-9].[0-5][0-9])$/
-        return pattern.test(value) || 'Invalid Time Format use M.SS'
+        return pattern.test(value) || "Invalid Time Format use M.SS";
       }
     ]
   }),
   methods: {
-    recalculatePace(){
-
-    }
+    recalculatePace() {}
   },
   computed: {
     convertMile2Km() {
-      let split = this.paceMile.split(".");
+      if(!pattern.test(this.paceMile)) return ""
+     
+      let split = this.paceMile.split(/[.,]/);
+      let pacePerMileInSecondes = parseInt(split[0]) * 60 + parseInt(split[1]);
+      let pacePerKmInSeconds = pacePerMileInSecondes * 0.62137119223733;
 
-      // let mile = parseFloat(this.paceMile)
-
-      // convertMile2Km mile to km
-      // 1.6 km = 1 mile
-
-      let paceMileInSecondes = parseInt(split[0]) * 60 + parseInt(split[1]);
-      let paceKmInSeconds = paceMileInSecondes * 0.62137119223733;
-
+      // create human readable string format: M.SS
       let convertedPaceInKm =
-        Math.floor(paceKmInSeconds / 60) +
+        Math.floor(pacePerKmInSeconds / 60) +
         "." +
-        (paceKmInSeconds % 60).toFixed(0);
+              // create human readable string format: M.SS
+        (pacePerKmInSeconds % 60).toFixed(0);
 
       return convertedPaceInKm;
     }
