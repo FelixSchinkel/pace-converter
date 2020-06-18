1d605d24e905772a49934434f3d9be2daf732016
Felix Schinkel <FelixSchinkel@users.noreply.github.com>
2020-06-18 11:43:35 +0200

    remove hello world convert now works in both directions

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,28 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app color="primary" dark>
+      <span class="mr-2">Convert Stuff</span>
+      <v-icon>mdi-open-in-new</v-icon>
     </v-app-bar>
 
     <v-content>
-      <ConvertPace/>
+      <ConvertPace />
     </v-content>
   </v-app>
 </template>
 
 <script>
-
-import ConvertPace from "./components/ConvertPace"
+import ConvertPace from "./components/ConvertPace";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    ConvertPace,
+    ConvertPace
   },
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
--- src/components/ConvertPace.vue
+++ src/components/ConvertPace.vue
@@ -2,9 +2,21 @@
   <v-card class="mx-auto">
     <v-card-text>
       <v-row>
-        <v-text-field label="pace/mile" v-model="paceMile" :rules="inputRules" type="number"/>
+        <v-text-field
+          label="pace/mile"
+          v-model="paceMile"
+          :rules="inputRules"
+          type="number"
+          @focus="setConvertionDirection(true)"
+        />
         <v-chip class="ma-2" label>&lt;-&gt;</v-chip>
-        <v-text-field label="pace/km" :value="convertMile2Km" :rules="inputRules" type="number"/>
+        <v-text-field
+          label="pace/km"
+          v-model="paceKm"
+          :rules="inputRules"
+          type="number"
+          @focus="setConvertionDirection(false)"
+        />
       </v-row>
     </v-card-text>
   </v-card>
@@ -16,7 +28,9 @@ const pattern = /^([0-9][.,][0-5][0-9])$/;
 
 export default {
   data: () => ({
+    convDirMiles2Km: true,
     paceMile: "",
+    paceKm: "",
     inputRules: [
       value => !!value || "Required.",
       value => {
@@ -24,25 +38,45 @@ export default {
       }
     ]
   }),
-  methods: {
-    recalculatePace() {}
+  watch: {
+    // determine convertion direction if input value changes
+    paceMile: function() {
+      if (this.convDirMiles2Km) {
+        this.paceKm = this.convertPace();
+      }
+    },
+    paceKm: function() {
+      if (!this.convDirMiles2Km) {
+        this.paceMile = this.convertPace();
+      }
+    }
   },
-  computed: {
-    convertMile2Km() {
-      if(!pattern.test(this.paceMile)) return ""
-     
-      let split = this.paceMile.split(/[.,]/);
-      let pacePerMileInSecondes = parseInt(split[0]) * 60 + parseInt(split[1]);
-      let pacePerKmInSeconds = pacePerMileInSecondes * 0.62137119223733;
+  methods: {
+    // set convertion direction base on selected input (either mile 2 km or km 2 mile)
+    setConvertionDirection: function(dir) {
+      this.convDirMiles2Km = dir;
+    },
+    // convert pace mile to km or other way arround if input is valid
+    convertPace: function() {
+      // based on which input elemt is selected convert pace mile-> km or the other way around
+      let pace = this.convDirMiles2Km === true ? this.paceMile : this.paceKm;
+      if (!pattern.test(pace)) return "";
+
+      let split = pace.split(/[.,]/);
+      let pacePerUnitInSecondes = parseInt(split[0]) * 60 + parseInt(split[1]); // Unit = km or mile depending an convDirMiles2Km
+
+      let converter =
+        this.convDirMiles2Km === true ? 0.62137119223733 : 1 / 0.62137119223733;
+
+      let pacePerUnitInSeconds = pacePerUnitInSecondes * converter;
 
       // create human readable string format: M.SS
-      let convertedPaceInKm =
-        Math.floor(pacePerKmInSeconds / 60) +
+      let convertedPace =
+        Math.floor(pacePerUnitInSeconds / 60) +
         "." +
-              // create human readable string format: M.SS
-        (pacePerKmInSeconds % 60).toFixed(0);
+        (pacePerUnitInSeconds % 60).toFixed(0);
 
-      return convertedPaceInKm;
+      return convertedPace;
     }
   }
 };
