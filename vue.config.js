09a707cd0e5a39820971419881522a7b5b57c4cf
Felix Schinkel <FelixSchinkel@users.noreply.github.com>
2020-07-20 11:01:54 +0200

    set correct vue deployment path for gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  /* github pages deployment path */
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/pace-converter/'
+  : '/'
 }
