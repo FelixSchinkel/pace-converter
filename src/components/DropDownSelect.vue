4c5a876e69f132a239219880c51ef40e2295a3a4
Felix Schinkel <FelixSchinkel@users.noreply.github.com>
2020-07-20 17:11:16 +0200

    add switch menu to select interval units

---
+++ src/components/DropDownSelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-btn text color="primary" @click="clickToggle">{{toggleItems}}</v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    items: Array, // max size = 2
+    value: String
+  },
+  data: () => ({
+    toggle: true
+  }),
+  computed: {
+    toggleItems: function() {
+      let val = "";
+      this.toggle ? (val = this.items[0]) : val = this.items[1];
+      this.$emit("input", val);
+      return val;
+    }
+  },
+  methods: {
+    clickToggle: function() {
+      this.toggle = !this.toggle;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/components/Intervall2Pace.vue
+++ src/components/Intervall2Pace.vue
@@ -4,17 +4,17 @@
     <v-card-subtitle>Define Intervall distance + time -> get Pace per km</v-card-subtitle>
     <v-card-text>
       <v-row>
-        <v-text-field label="distance" v-model="distance" class="mx-4" type="tel"/>
-        <v-radio-group v-model="distanceUnit" row>
-          <v-radio label="m" value="m"></v-radio>
-          <v-radio label="km" value="km"></v-radio>
-        </v-radio-group>
+        <v-text-field label="distance" v-model="distance" class="mx-4" type="tel">
+          <template v-slot:append-outer>
+            <DropDownSelect :items="distanceUnitOptions" v-model="distanceUnit" />
+          </template>
+        </v-text-field>
         <v-icon>mdi-plus</v-icon>
-        <PaceInput label="time" v-model="time" />
-        <v-radio-group v-model="timeUnit" row>
-          <v-radio label="seconds" value="s"></v-radio>
-          <v-radio label="minutes + seconds" value="ms"></v-radio>
-        </v-radio-group>
+        <v-text-field class="mx-4" label="time" v-model="time">
+          <template v-slot:append-outer>
+            <DropDownSelect :items="timeUnitOptions" v-model="timeUnit" />
+          </template>
+        </v-text-field>
         <v-icon>mdi-equal</v-icon>
         <v-text-field label="pace/km" v-model="pace" readonly class="mx-4" />
       </v-row>
@@ -23,41 +23,50 @@
 </template>
 
 <script>
-import PaceInput from "./PaceInput";
+// import PaceInput from "./PaceInput";
+import DropDownSelect from "./DropDownSelect";
 
 const pattern = /^([0-9][.][0-5][0-9])$/;
 
 export default {
-  components: { PaceInput },
+  components: { /* PaceInput ,*/ DropDownSelect },
   data: () => ({
     distance: "",
     distanceUnit: "m",
+    distanceUnitOptions: ["m", "km"],
+
     time: "",
-    timeUnit: "ms"
+    timeUnit: "ms",
+    timeUnitOptions: ["seconds", "minutes + seconds"]
   }),
   computed: {
     pace: function() {
       if (!this.distance && !this.time) return;
       if (this.timeUnit === "ms" && !pattern.test(this.time)) return;
       // convert distance to meters and time to seconds
-      
+
       let dist;
       if (this.distanceUnit === "km") dist = this.distance * 1000;
       else dist = this.distance;
 
       let seconds;
-      if (this.timeUnit === "ms") {
+      //if input in minutes -> convert to seconds
+      if (this.timeUnit === "minutes + seconds") {
         let timeSplit;
         if (this.time.includes(".")) {
           timeSplit = this.time.split(".");
-          if (timeSplit[1].length === 1) timeSplit[1] = timeSplit[1] + "0";
-        } else timeSplit = [this.time, 0];
+          // if input like '3.' add '0 ' to fix conversation to seconds   
+          if (timeSplit[1].length === 0) timeSplit[1] = "0";
+          console.log("timepslit", timeSplit);
+        } else {
+          timeSplit = [this.time, 0];
+        }
 
         seconds = parseInt(timeSplit[0] * 60) + parseInt(timeSplit[1]);
       } else seconds = this.time;
 
-      // calc pace
-      let pace = (1000 * seconds) / dist;
+      // calc pace - floor to int
+      let pace = Math.floor((1000 * seconds) / dist);
 
       return (
         Math.floor(pace / 60) + "m" + (pace % 60) + "s (" + pace + " seconds)"
